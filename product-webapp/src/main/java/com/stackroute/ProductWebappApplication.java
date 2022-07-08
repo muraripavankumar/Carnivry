170f20cba97f224ccdeb0604d3c2e552f3e0d02a
Shanmukh Reddy <mandapati.reddy@niit.com>
2022-07-08 11:40:04 +0530

    initial commit

---
+++ product-webapp/src/main/java/com/stackroute/ProductWebappApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductWebappApplication.class, args);
+	}
+
+}
