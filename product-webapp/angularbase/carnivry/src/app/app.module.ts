f6f096ce3f385346c462816e0922885cf89468c4
Mayukh Maity <maitymayukh23@gmail.com>
2022-07-14 02:02:40 +0530

    mm_eventform_eventdetails

--- product-webapp/angularbase/carnivry/src/app/app.module.ts
+++ product-webapp/angularbase/carnivry/src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HostEventComponent } from './management-service/host-event/host-event.component';
 
+import {MatStepperModule} from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +23,16 @@ import { HostEventComponent } from './management-service/host-event/host-event.c
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatIconModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- product-webapp/angularbase/carnivry/src/app/management-service/host-event/host-event.component.ts
+++ product-webapp/angularbase/carnivry/src/app/management-service/host-event/host-event.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
+// import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { ElementRef, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+// import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-host-event',
@@ -6,10 +17,95 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./host-event.component.css']
 })
 export class HostEventComponent implements OnInit {
+  firstFormGroup = this._formBuilder.group({
+    title: ['', Validators.required],
+    eventDescription: ['', Validators.required, Validators.minLength(50)],
+    artist: [''],
+    genre: [''],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {
+    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+      startWith(null),
+      map((fruit: string | null) => (fruit ? this._filter(fruit) : this.allFruits.slice())),
+    );
+   }
 
   ngOnInit(): void {
   }
 
+  /////////////////////////////////////////////////////////
+  addOnBlur = true;
+  // readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  // fruits: Fruit[] = [{name: 'Lemon'}, {name: 'Lime'}, {name: 'Apple'}];
+  artists: string[];
+
+  addArtist(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add artist
+    if (value) {
+      this.artists.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  removeArtist(artist: string): void {
+    const index = this.artists.indexOf(artist);
+
+    if (index >= 0) {
+      this.artists.splice(index, 1);
+    }
+  }
+  ///////////////////////////////////////////////////////
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruitCtrl = new FormControl('');
+  filteredFruits: Observable<string[]>;
+  fruits: string[] = ['Lemon'];
+  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+
+  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+
+  // constructor() {}
+
+  addGenre(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.fruitCtrl.setValue(null);
+  }
+
+  removeGenre(fruit: string): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.fruits.push(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
+    this.fruitCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allFruits.filter(fruit => fruit.toLowerCase().includes(filterValue));
+  }
+  /////////////////////////////////////////////////////////////
 }
