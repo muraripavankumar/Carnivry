97ad330e9bcda5f7286f11bef80d458dc3a0e193
Vanquishr <saumasischandra987@gmail.com>
2022-07-27 15:58:47 +0530

    profile pic error solving

--- authentication-service/src/main/java/com/stackroute/rabbitmq/Consumer.java
+++ authentication-service/src/main/java/com/stackroute/rabbitmq/Consumer.java
@@ -7,16 +7,16 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//@Component
-//public class Consumer {
-//    @Autowired
-//    private UserService userService;
-//
-//    @RabbitListener(queues = "queue_1")
-//    public void getDataAndAddToDatabase(UserDTO userDTO) throws UserAlreadyExistsException {
-//        User user=new User();
-//        user.setEmail(userDTO.getEmail());
-//        user.setPassword(userDTO.getPassword());
-//        userService.saveUser(user);
-//    }
-//}
+@Component
+public class Consumer {
+    @Autowired
+    private UserService userService;
+
+    @RabbitListener(queues = "queue_1")
+    public void getDataAndAddToDatabase(UserDTO userDTO) throws UserAlreadyExistsException {
+        User user=new User();
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        userService.saveUser(user);
+    }
+}
