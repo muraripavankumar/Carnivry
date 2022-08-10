f3e13be3d1e110bbc7151a76f13c7688c9135798
Mayukh Maity <maitymayukh23@gmail.com>
2022-08-10 13:15:59 +0530

    updated eventSuggestionDTO in management service

--- management-service/src/main/java/com/stackroute/modelDTO/EventSuggestionsDTO.java
+++ management-service/src/main/java/com/stackroute/modelDTO/EventSuggestionsDTO.java
@@ -13,26 +13,13 @@ public class EventSuggestionsDTO {
     private String title;
     private String eventType;
     private String userEmailId;
-    private String eventDescription;
-    private List<String> artists;
     private List<String> genre;
     private List<String> languages;
     private Date startDate;
     private Date endDate;
-    private String startTime;
-    private String endTime;
     private String poster;
-    private String venue;
-    private String houseNo;
-    private String street;
-    private String landmark;
     private String city;
-    private String state;
-    private int pincode;
     private int ticketsSold;
-    private Double revenueGenerated;
     private Double price;
     private int totalSeats;
-    private int likes;
-    private List<String> emailOfUsersLikedEvent;
 }
