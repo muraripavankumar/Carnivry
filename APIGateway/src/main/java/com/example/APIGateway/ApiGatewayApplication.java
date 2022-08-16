cf3d34bb651dd9757749dda2a332d64d9fe4b0be
Mayukh Maity <maitymayukh23@gmail.com>
2022-08-16 09:32:15 +0530

    enhanced the host event ui

--- APIGateway/src/main/java/com/example/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/example/APIGateway/ApiGatewayApplication.java
@@ -4,13 +4,13 @@ package com.example.APIGateway;
 //import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.core.Ordered;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
