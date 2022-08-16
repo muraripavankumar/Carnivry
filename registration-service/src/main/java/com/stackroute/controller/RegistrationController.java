2c0c7db31cefa4ebd1f45de15631690348f9f449
Shanmukh Reddy <mandapati.reddy@niit.com>
2022-08-16 09:38:53 +0530

    webapp build

--- registration-service/src/main/java/com/stackroute/controller/RegistrationController.java
+++ registration-service/src/main/java/com/stackroute/controller/RegistrationController.java
@@ -486,10 +486,10 @@ public class RegistrationController {
     private String applicationUrl(HttpServletRequest request) {
 //        System.out.println(request.getHeader("Referer"));
         log.debug("application url generated");
-        return "http://" +
-                request.getServerName() +
-                ":" +
-                request.getServerPort() +
+        return "https://carnivry.stackroute.io" +
+//                request.getServerName() +
+//                ":" +
+//                request.getServerPort() +
                 request.getContextPath();
     }
 }
